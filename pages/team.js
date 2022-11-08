2849f6bbbce87d664465faebfa4cd9ddbba3046f
tran0460 <tran0460@algonquinlive.com>
2022-11-07 20:24:28 -0500

    display team data

--- pages/team.js
+++ pages/team.js
@@ -1,1 +1,19 @@
-export default function Team({ team }) {}
+import { useAppContext } from "../Context/AppContext";
+
+export default function Team() {
+  const { teamList } = useAppContext();
+  return (
+    <div>
+      {teamList.map((member) => (
+        <div key={member.id}>
+          <p>{member.fullName}</p>
+          <p>{member.age}</p>
+          <p>{member.role}</p>
+          <p>{member.title}</p>
+          <p>{member.level}</p>
+          <p>***************</p>
+        </div>
+      ))}
+    </div>
+  );
+}
