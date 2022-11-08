a48882dc9d6ac47789ba0eb2c76419f6fc49a2d9
tran0460 <tran0460@algonquinlive.com>
2022-11-07 20:04:06 -0500

    import context

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+import { useState } from "react";
+import { AppContext } from "../Context/AppContext";
 
 function MyApp({ Component, pageProps }) {
+  const [teamList, setTeamList] = useState([]);
+  const contextValue = [teamList, setTeamList];
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AppContext.Provider value={contextValue}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AppContext.Provider>
   );
 }
 
