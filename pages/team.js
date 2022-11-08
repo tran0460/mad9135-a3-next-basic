bf7f28e71cb5b028a21c902c59428e7740482be0
tran0460 <tran0460@algonquinlive.com>
2022-11-07 20:45:32 -0500

    display image

--- pages/team.js
+++ pages/team.js
@@ -1,19 +1,29 @@
 import { useAppContext } from "../Context/AppContext";
+import Image from "next/image";
 
 export default function Team() {
   const { teamList } = useAppContext();
   return (
     <div>
-      {teamList.map((member) => (
-        <div key={member.id}>
-          <p>{member.fullName}</p>
-          <p>{member.age}</p>
-          <p>{member.role}</p>
-          <p>{member.title}</p>
-          <p>{member.level}</p>
-          <p>***************</p>
-        </div>
-      ))}
+      {teamList.map((member) => {
+        return (
+          <div key={member.id}>
+            <Image
+              src={member.imageSrc}
+              alt="me"
+              width="200"
+              height="200"
+              style={{ objectFit: "cover" }}
+            />
+            <p>{member.fullName}</p>
+            <p>{member.age}</p>
+            <p>{member.role}</p>
+            <p>{member.title}</p>
+            <p>{member.level}</p>
+            <p>***************</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
