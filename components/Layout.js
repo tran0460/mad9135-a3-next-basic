d12890696a94d47db3ff39485177ca2d85b66402
tran0460 <tran0460@algonquinlive.com>
2022-11-08 17:10:12 -0500

    add header

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,10 @@
+import Header from "./Header";
+
 const Layout = (props) => {
   const children = props.children;
   return (
     <div className="content">
+      <Header />
       {children} {/** To load <Component/> in _app.js */}
     </div>
   );
