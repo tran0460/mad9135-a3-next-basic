459b76bfd3385d508ce39b10d156c92f922519ff
tran0460 <tran0460@algonquinlive.com>
2022-11-07 19:55:56 -0500

    create data

---
+++ datasource/Data.js
@@ -0,0 +1,30 @@
+export const data = [
+  {
+    id: Date.now(),
+    fullName: "Antonio Pugh",
+    age: 29,
+    role: "Human Team Engineer",
+    level: "Intermediate",
+  },
+  {
+    id: Date.now(),
+    fullName: "Stacie Eaton",
+    age: 19,
+    role: "Global Identity Director",
+    level: "Entry",
+  },
+  {
+    id: Date.now(),
+    fullName: "Aayush Wormald",
+    age: 35,
+    role: "Global Communications Engineer",
+    level: "Senior",
+  },
+  {
+    id: Date.now(),
+    fullName: "Jimmie Andrews",
+    age: 47,
+    role: "Regional Configuration Planner",
+    level: "Intermediate",
+  },
+];
