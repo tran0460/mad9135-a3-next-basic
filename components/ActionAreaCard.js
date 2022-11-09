8cfad0f90d8f50d828d4033351a28ad685f08b04
tran0460 <tran0460@algonquinlive.com>
2022-11-08 20:05:09 -0500

    make team into card component

--- components/ActionAreaCard.js
+++ components/ActionAreaCard.js
@@ -5,8 +5,9 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 import Link from "next/link";
+import Image from "next/image";
 
-export default function ActionAreaCard({ data }) {
+export default function ActionAreaCard({ data, type }) {
   const randImg = `https://picsum.photos/id/${(Math.random() * 100).toFixed(
     0
   )}/500/500`;
@@ -18,15 +19,44 @@ export default function ActionAreaCard({ data }) {
           query: { randImg },
         }}>
         <CardActionArea>
-          <CardMedia component="img" image={randImg} alt="image" />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {data.title}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {data.body}
-            </Typography>
-          </CardContent>
+          {type == "project" ? (
+            <>
+              <CardMedia component="img" image={randImg} alt="image" />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {data.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {data.body}
+                </Typography>
+              </CardContent>
+            </>
+          ) : (
+            <>
+              {" "}
+              <Image
+                src={data.imageSrc}
+                alt="me"
+                width="345"
+                height="345"
+                style={{ objectFit: "cover" }}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {data.fullName}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Age: {data.age}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Job Title: {data.role}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Level: {data.level}
+                </Typography>
+              </CardContent>
+            </>
+          )}
         </CardActionArea>
       </Link>
     </Card>
--- pages/team.js
+++ pages/team.js
@@ -1,29 +1,29 @@
 import { useAppContext } from "../Context/AppContext";
 import Image from "next/image";
+import ActionAreaCard from "../components/ActionAreaCard";
+import { Container } from "@mui/material";
 
 export default function Team() {
   const { teamList } = useAppContext();
   return (
-    <div>
+    <Container
+      maxWidth="lg"
+      sx={{
+        display: "flex",
+        flexDirection: { sx: "column", md: "row" },
+        flexWrap: "wrap",
+        alignItems: "stretch",
+        justifyContent: "center",
+        gap: "2rem",
+        padding: "2rem",
+      }}>
       {teamList.map((member) => {
         return (
           <div key={member.id}>
-            <Image
-              src={member.imageSrc}
-              alt="me"
-              width="200"
-              height="200"
-              style={{ objectFit: "cover" }}
-            />
-            <p>{member.fullName}</p>
-            <p>{member.age}</p>
-            <p>{member.role}</p>
-            <p>{member.title}</p>
-            <p>{member.level}</p>
-            <p>***************</p>
+            <ActionAreaCard data={member} />
           </div>
         );
       })}
-    </div>
+    </Container>
   );
 }
